2e743a4ad42a26397b2e331131b07a497d24e0be
nfour <novus.nfour@gmail.com>
2016-08-10 16:19:07 +1000

    Split serverless layouts, create minimal v1.0 layout

---
+++ layouts/serverless_v0.5__SUBSET/backend/functions/foo/handler.js
@@ -0,0 +1,9 @@
+import { FOO } from '../../lib/foo'
+
+export function handler(event, context, done) {
+    done = done || context.done
+
+    return done(null, {
+        message: JSON.stringify(FOO)
+    })
+}
